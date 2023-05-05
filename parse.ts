176390cc5820126580fe6a7c0ab68410e21aa4d7
mryhryki <mryhryki@gmail.com>
2023-05-05 11:53:02 +0900

    Update parse.ts

--- parse.ts
+++ parse.ts
@@ -7,27 +7,33 @@ async function main() {
     path.resolve(__dirname, "./dist/index.js.map"),
   );
   const sourceMap = JSON.parse(rawSourceMap.toString("utf-8"));
+  const { sources } = sourceMap;
 
-  const current = [0, 0, 0, 0, 0]
+  const current = [0, 0, 0, 0, 0];
   const merge = (arr: number[]) => {
-    arr.forEach((v, i) => current[i] += v)
-    return current.slice(0, arr.length)
-  }
-  const mappings = sourceMap.mappings.split(";").map((line, lineNumber) => ({
-    lineNumber,
-    mappings: line.split(",").map((str: string) => {
+    arr.forEach((v, i) => current[i] += v);
+    return current.slice(0, Math.max(arr.length, 4));
+  };
+
+  const mappings = sourceMap.mappings.split(";").map((line, lineNumber) =>
+    line.split(",").map((str: string) => {
       const [i0, i1, i2, i3, i4] = merge(vlq.decode(str));
-      return {
-        "変換後の列番号": i0,
-        "変換前のファイル名のIndex": i1,
-        "変換前のファイルの行番号": i2,
-        "変換後のファイルの列番号": i3,
-        "変換後の単語のIndex": i4,
-        "変換後の単語": i4 == null ? undefined : sourceMap.names[i4],
-      };
-    }),
-  }));
-  console.log(JSON.stringify({mappings}, null, 2));
+      const word = i4 == null ? "" : ` (${sourceMap.names[i4]})`;
+      return [
+        `L${lineNumber + 1}:C${i0 + 1}`.padEnd(7, ' '), //
+        `${sources[i1]}:L${i2 + 1}:C${i3 + 1}${word}`,
+      ].join(" => ");
+      // return {
+      //   "変換後の列番号": i0,
+      //   "変換前のファイル名のIndex": i1,
+      //   "変換前のファイルの行番号": i2,
+      //   "変換後のファイルの列番号": i3,
+      //   "変換後の単語のIndex": i4,
+      //   "変換後の単語": i4 == null ? undefined : sourceMap.names[i4],
+      // };
+    })
+  ).flat();
+  console.log(JSON.stringify(mappings, null, 2));
 }
 
 main().catch(console.error);
